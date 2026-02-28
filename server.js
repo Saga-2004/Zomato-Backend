249a0bae4a85db009dca09562a0f84f13627dd65
Sagar <msagars2008@gmail.com>
2026-02-28 10:33:00 +0530

    Add api

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend URL
+    origin: process.env.FRONTEND_URL || "http://localhost:5173", // frontend URL
     credentials: true,
   }),
 );
